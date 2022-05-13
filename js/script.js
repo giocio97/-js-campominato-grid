4319f75b4549bb731896285d5a9be5ef018b8ca5
Giovanni Ciotta <giociotta@gmail.com>
2022-05-13 16:18:34 +0200

    starter campo minato

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+const livello = document.getElementById(`difficoltà`);
+const button = document.getElementById (`invio`);
+const griglia = document.getElementById (`griglia`);
+
+button.addEventListener(`click`,
+// L’utente indica un livello di difficoltà 
+)
+
+
+// con difficoltà 1 => tra 1 e 100
+// con difficoltà 2 => tra 1 e 81
+// con difficoltà 3 => tra 1 e 49
+// Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
